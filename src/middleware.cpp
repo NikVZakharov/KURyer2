714b63079d096c34c7072cca60ceee7e1671bfca
NikitaZakharov <zalharovznv@gmail.com>
2024-01-25 18:18:33 +0300

    Энкодер

--- src/middleware.cpp
+++ src/middleware.cpp
@@ -45,7 +45,7 @@ bool isOnBlack(int sensor)
 bool isOnCross()
 {
   bool result = false;
-  if (isOnBlack(IR_SENSOR_L_PIN) && isOnBlack(IR_SENSOR_R_PIN))
+  if (isOnBlack(8) && isOnBlack(9))
   {
     result = true;
   }
